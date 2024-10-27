ac16893d3dbe8d685239515511938cbc2b44185c
thomasampomah <ampomathomas1991@gmail.com>
2024-10-27 20:29:43 +0000

    task0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,11 +3,12 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost", 
     user="taopoku", 
-    password="Tom0244827889!!!",
-    database="alx_book_store")
+    password="Tom0244827889!!!",)
 
 
 mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
 mycursor.execute("""CREATE TABLE Books (
     book_id INT PRIMARY KEY,
