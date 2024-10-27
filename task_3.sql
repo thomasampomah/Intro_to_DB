4d7e02c2dc50e1dd64ca0896e38922ae21dd0761
thomasampomah <ampomathomas1991@gmail.com>
2024-10-27 22:05:38 +0000

    task3

---
+++ task_3.sql
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost", 
+    user="taopoku", 
+    password="Tom0244827889!!!",)
+
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW * FROM alx_book_store")
+
+mycursor.close()
+mydb.close()
