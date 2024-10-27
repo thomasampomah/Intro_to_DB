118a72ab1c78c0b90f4e49979e6f57ca94b2296d
thomasampomah <ampomathomas1991@gmail.com>
2024-10-27 21:29:15 +0000

    task1

--- MySQLServer.py
+++ MySQLServer.py
@@ -8,9 +8,12 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-
-print("Database alx_book_store created successfully!")
+try:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database alx_book_store created successfully!")
+except:
+    mysql.connector.Error
+    print("Error in connection")
 
 
 mycursor.close()
