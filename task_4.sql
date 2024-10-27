f1613412f608a6c5413d4f192f117a4e50a47ee4
thomasampomah <ampomathomas1991@gmail.com>
2024-10-27 22:42:49 +0000

    task4

--- task_4.sql
+++ task_4.sql
@@ -8,7 +8,12 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SHOW * FROM Books");
+mycursor.execute("""INFORMATION_SCHEMA.COLUMNS", 
+"SELECT", 
+"COLUMN_NAME", 
+"COLUMN_TYPE", 
+"TABLE_SCHEMA = 'alx_book_store'", 
+"TABLE_NAME = 'Books""");
 
 mycursor.close()
 mydb.close()
