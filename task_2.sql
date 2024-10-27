f645c02739b58dfecbef6f0d67abbb671955b2e7
thomasampomah <ampomathomas1991@gmail.com>
2024-10-27 21:57:01 +0000

    task2

--- task_2.sql
+++ task_2.sql
@@ -7,12 +7,37 @@ mydb = mysql.connector.connect(
     database="alx_book_store")
 
 
-mycursor = mydb.cursor()
+mycursor.execute("""CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id (Foreign Key referencing Authors table),
+    price DOUBLE,
+    publication_date DATE""");
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
-mycursor.execute("CREATE TABLE books, 
-    authors, customers, orders, order details")
+mycursor.execute("""CREATE TABLE Authors (
+    author_id (Primary Key)
+    author_name VARCHAR(215))""");
+
+
+mycursor.execute("""CREATE TABLE Customers(
+    customer_id (Primary Key)
+    customer_name VARCHAR(215)
+    email VARCHAR(215)
+    address TEXT)""");
+
+mycursor.execute("""CREATE TABLE Orders (
+    order_id INT (Primary Key)
+    customer_id INT ["FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)"]
+    order_date DATE)""");
+
+
+mycursor.execute("""CREATE TABLE Order_Details(
+    orderdetailid (Primary Key)
+    order_id ["FOREIGN KEY (order_id) REFERENCES Orders(order_id)"]
+    book_id ["FOREIGN KEY (book_id) REFERENCES Books(book_id)"
+    quantity DOUBLE)""");
+
 
 mycursor.close()
 mydb.close()
