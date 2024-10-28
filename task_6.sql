fb37861c51027038597d764c15538eedd9bdb308
thomasampomah <ampomathomas1991@gmail.com>
2024-10-28 00:06:44 +0000

    task6

--- task_6.sql
+++ task_6.sql
@@ -13,8 +13,8 @@ mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store");
 
 db = INSERT INTO customer (customer_id, customer_name, email, address); 
 val = VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
-    (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
-    (4, "Nehemial Kamolu","`nkamolu@sandtech.com","126 Happiness Ave.")
+    (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+    (4, "Nehemial Kamolu","`nkamolu@sandtech.com","126 Happiness  Ave.")
 
 mycursor.execute(db, val)
 mydb.commit()  
